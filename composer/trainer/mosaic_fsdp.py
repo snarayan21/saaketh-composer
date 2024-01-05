1622ef0dc919404d68be701b111f2f45785195eb
Saaketh <narayan.saaketh@gmail.com>
2024-01-05 17:56:12 -0500

    use new patching func

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -81,33 +81,33 @@ def patch_pytorch():
         from composer.trainer.mosaic_fsdp_utils import _share_state_and_init_handle_attrs_t2p2
         _runtime_utils._share_state_and_init_handle_attrs = _share_state_and_init_handle_attrs_t2p2
 
-def patch_compressed_collectives(compress_fn: Callable,
-                                 decompress_fn: Callable,
-                                 compress_kwargs: Optional[dict] = None,
-                                 decompress_kwargs: Optional[dict] = None):
-    """Monkey patches specific collective operations. Currently only all gather for FSDP."""
-    if version.parse(torch.__version__) < version.parse('2.1.0'):
-        raise NotImplementedError(f'8 bit all gather not supported for torch < 2.1.0')
-    elif version.parse(torch.__version__) >= version.parse('2.1.1'):
-        raise NotImplementedError(f'8 bit all gather not supported for torch >= 2.2.0')
-    else:
-        # Monkey patch _allgather_base in ProcessGroup to use 8 bits.
-        from torch.distributed import ProcessGroup
+# def patch_compressed_collectives(compress_fn: Callable,
+#                                  decompress_fn: Callable,
+#                                  compress_kwargs: Optional[dict] = None,
+#                                  decompress_kwargs: Optional[dict] = None):
+#     """Monkey patches specific collective operations. Currently only all gather for FSDP."""
+#     if version.parse(torch.__version__) < version.parse('2.1.0'):
+#         raise NotImplementedError(f'8 bit all gather not supported for torch < 2.1.0')
+#     elif version.parse(torch.__version__) >= version.parse('2.1.1'):
+#         raise NotImplementedError(f'8 bit all gather not supported for torch >= 2.2.0')
+#     else:
+#         # Monkey patch _allgather_base in ProcessGroup to use 8 bits.
+#         from torch.distributed import ProcessGroup
 
-        collectives_names = ['_allgather_base']
-        for name in collectives_names:
-            collective = getattr(ProcessGroup, name)
+#         collectives_names = ['_allgather_base']
+#         for name in collectives_names:
+#             collective = getattr(ProcessGroup, name)
 
-            # Compress before and decompress after using the provided functions.
-            @functools.wraps(collective)
-            def compressed_collective(*args, **kwargs):
-                comp_coll = CompressedCollective(compress_fn,
-                                        decompress_fn,
-                                        compress_kwargs,
-                                        decompress_kwargs)
-                return comp_coll.call(collective, *args, **kwargs)
+#             # Compress before and decompress after using the provided functions.
+#             @functools.wraps(collective)
+#             def compressed_collective(*args, **kwargs):
+#                 comp_coll = CompressedCollective(compress_fn,
+#                                         decompress_fn,
+#                                         compress_kwargs,
+#                                         decompress_kwargs)
+#                 return comp_coll.call(collective, *args, **kwargs)
             
-            setattr(ProcessGroup, name, compressed_collective)
+#             setattr(ProcessGroup, name, compressed_collective)
 
 def patch_compressed_collectives(compress_fn: Callable,
                                  decompress_fn: Callable,
